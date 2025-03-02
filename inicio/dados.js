8c47e9ab6d17fe56489562db2b45031241868449
LagaMBa <murilo_mls@hotmail.com>
2025-03-02 13:45:55 -0300

    alteracao_1

--- inicio/dados.js
+++ inicio/dados.js
@@ -2,9 +2,11 @@ var quantidade = 10;
 var preco = 6.4;
 var imposto = 1.5;
 var precoFinal = preco + imposto;
+var nome = "Caneta";
 
 console.log("Caneta");
 console.log(quantidade); // valor literal
 console.log(preco);
 console.log(1.5);
 console.log(precoFinal);
+console.log(nome);
