0178f99ae86ee042be44cbe4fa78c08b92c42818
Lagamba <murilo_mls@hotmail.com>
2025-03-02 12:15:21 -0300

    First Commit

---
+++ inicio/comentario.js
@@ -0,0 +1,10 @@
+// comentario 
+
+console.log("Ola")
+
+/* Varios
+COmentarios
+Aqui
+*/ 
+
+console.log("Bem vindo!")
---
+++ inicio/dados.js
@@ -0,0 +1,9 @@
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+
+console.log("Caneta");
+console.log(quantidade); // valor literal
+console.log(preco);
+console.log(1.5);
+console.log(7.9);
