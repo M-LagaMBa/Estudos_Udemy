41c31995d91a150c41cefe26d585e84e30a12bfe
LagaMBa <murilo_mls@hotmail.com>
2025-03-04 11:04:31 -0300

    dados_3

---
+++ inicio/dados_2.js
@@ -0,0 +1,17 @@
+let preco = 19.90;
+let desconto = 0.4;
+
+console.log(19.9 * 0.6);
+
+let precoComdesconto = preco * (1 - desconto);
+console.log(precoComdesconto);
+
+let nome = "Caderno"; // String (Texto) -> sequencia de simbolos
+let categoria = "Papelaria";
+console.log("Produto: " + nome
+    + ", Categoria: " + categoria
+    + ", Preço: " + preco
+    + ", Desconto: " + desconto);
+
+console.log(1 + 1);
+console.log("1" + 1);
