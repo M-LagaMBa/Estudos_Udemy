c3ecf5786cfeb4e8b575e80169ec908b68e7d262
LagaMBa <murilo_mls@hotmail.com>
2025-03-16 12:20:29 -0300

    desafio_logicos

---
+++ operadores/desafio_logicos.js
@@ -0,0 +1,15 @@
+// Duas expressoes resultar em TRUE (Aritimeticos/Relacionais/Logicos)
+
+let verdadeira1 = 2 + 5 >=7 && 8 > 2;
+let verdadeira2 = !(6 * 6 < 3);
+
+console.log(verdadeira1);
+console.log(verdadeira1);
+
+// Duas expressoes resultar em FALSE (Aritimeticos/Relacionais/Logicos)
+
+let falso1 = 8 - 4 > 12 || false || 15 === '15';
+let falso2 = !(7 !== 7 || "3" !== 3);
+
+console.log(falso1);
+console.log(falso2);
